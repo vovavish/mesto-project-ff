ebbd874b930273c120050d6b1f1c5ab465ae874a
vovavish <vovavishniakov856@gmail.com>
2024-07-28 23:57:33 +0300

    added create and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const cardTemplate = document
+  .querySelector('#card-template')
+  .content.querySelector('.card');
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(card, deleteCallback) {
+  const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-// @todo: Вывести карточки на страницу
+  cardElement
+    .querySelector('.card__delete-button')
+    .addEventListener('click', deleteCallback);
+
+  return cardElement;
+}
+
+const deleteCardCallback = evt => evt.target.parentElement.remove();
+
+initialCards.forEach(card =>
+  placesList.append(createCard(card, deleteCardCallback))
+);
